f298c969f055f4685fad5ba801cb46981863eead
xsmd <3514862572@qq.com>
2022-09-24 11:07:41 +0800

    初始化

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Change Started!');
+// ==UserScript==
+// @name         XJOJ Nightmode
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  A Darkmode JS for XJOJ
+// @author       XSMD
+// @match        http://115.236.49.52:8000/
+// @icon         http://115.236.49.52:8000/images/logo_small.png
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    console.log("Started!");
+
+})();

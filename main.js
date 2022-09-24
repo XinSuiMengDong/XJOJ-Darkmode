6b2eb457048c5649562e79aa984348937d352b4e
xsmd <3514862572@qq.com>
2022-09-24 11:20:23 +0800

    A little.

--- main.js
+++ main.js
@@ -7,10 +7,26 @@
 // @match        http://115.236.49.52:8000/
 // @icon         http://115.236.49.52:8000/images/logo_small.png
 // @grant        none
+// @license GPLv3
 // ==/UserScript==
 
 (function () {
     'use strict';
     console.log("Started!");
-
+    let DkEnable = localStorage.getItem('DKEnable') === 'true';
+    let btnArea = document.querySelector("body");
+    let btn = document.createElement("button");
+    btn.textContent = DkEnable ? "Dark" : "Light";
+    btn.addEventListener("click", () => {
+        DkEnable = !DkEnable;
+        localStorage.setItem("DKEnable", DkEnable);
+        btn.textContent = DkEnable ? "Dark" : "Light";
+        if (DkEnable) {
+            //Dark mode
+            console.log('Dark mode')
+        } else {
+            //Light mode
+            location.reload();
+        }
+    });
 })();

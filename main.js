9f853aae94a5b2aba2357ed3e4771f8f71f07bd6
xsmd <3514862572@qq.com>
2022-09-24 14:02:40 +0800

    Nearly complete.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  A Darkmode JS for XJOJ
 // @author       XSMD
-// @match        http://115.236.49.52:8000/
+// @match        http://115.236.49.52:8000/*
 // @icon         http://115.236.49.52:8000/images/logo_small.png
 // @grant        none
 // @license GPLv3
@@ -15,29 +15,49 @@
     console.log("Started!");
     // localStorage.setItem('DkEnable', 'false');
     let DkEnable = localStorage.getItem('DkEnable');
-    function Dkmode {
+
+    //定义函数
+    function Dkmode() {
+        //alert('Dark' + String(DkEnable));
+        DkEnable = 'true';
         document.querySelector("body").setAttribute('style', 'background-color: #000; color: #FFF;');
-        document.querySelector("body > div.container.theme-showcase > div.uoj-content > div.row > div > div.table-responsive > table").setAttribute('style', 'color: #FFF;');
+        try {
+            document.querySelector("body > div.container.theme-showcase > div.uoj-content > div.row > div > div.table-responsive > table").setAttribute('style', 'color: #FFF;');
+        } catch (error) {
+            return;
+        }
+
     };
-    function Ltmode {
+    function Ltmode() {
+        //alert('Light' + String(DkEnable));
+        DkEnable = 'false';
         document.querySelector("body").setAttribute('style', 'background-color: #FFF; color: #111111;');
+        try {
+            document.querySelector("body > div.container.theme-showcase > div.uoj-content > div.row > div > div.table-responsive > table").setAttribute('style', 'color: #000;');
+        } catch (error) {
+            return;
+        }
+
     };
 
     //初始化
-    document.querySelector("body > div.container.theme-showcase > div.uoj-content > div.card.card-default > div").remove();
-    if (DkEnable) Dkmode();
+    try {
+        document.querySelector("body > div.container.theme-showcase > div.uoj-footer").remove();
+        document.querySelector("body > div.container.theme-showcase > div.uoj-content > div.card.card-default > div").remove();
+    } catch (error) {
+
+    }
+    if (DkEnable == 'true') Dkmode();
     else Ltmode();
 
     let btn = document.createElement('button');
     btn.setAttribute('stlye', 'position: relative; top: 300px; right: 0px; height: 30px; width: 30px');
-    btn.textContent = DkEnable ? "Light" : "Dark";
+    btn.textContent = DkEnable == 'true' ? "Light" : "Dark";
     btn.addEventListener('click', () => {
-        DkEnable = !DkEnable;
-        btn.textContent = DkEnable ? "Light" : "Dark";
-        localStorage.setItem('DkEnable', String(DkEnable));
-        body = document.querySelector("body");
-        if (DkEnable) Dkmode();
-        else Ltmode();
+        if (DkEnable == 'true') Ltmode();
+        else Dkmode();
+        btn.textContent = DkEnable == 'true' ? "Light" : "Dark";
+        localStorage.setItem('DkEnable', DkEnable);
     })
     document.body.appendChild(btn);
 })();
